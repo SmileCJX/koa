e2915b99dc38968fe418729035d6f6bcc43f5a1a
SmileCJX <1092925421@qq.com>
2017-05-20 14:34:11 +0800

    处理post请求

--- app-router.js
+++ app-router.js
@@ -1,5 +1,7 @@
 const Koa = require('koa');
 
+
+const bodyParser = require('koa-bodyparser');
 // 注意require('koa-router')返回的是函数:
 const router = require('koa-router')();
 
@@ -18,11 +20,31 @@ router.get('/hello/:name', async (ctx, next) => {
 });
 
 router.get('/', async (ctx, next) => {
-    ctx.response.body = '<h1>Index</h1>';
+    ctx.response.body = `<h1>Index</h1>
+        <form action="/signin" method="post">
+            <p>Name: <input name="name" value="koa"></p>
+            <p>Password: <input name="password" type="password"></p>
+            <p><input type="submit" value="Submit"></p>
+        </form>`;
 });
 
+router.post('/signin',async(ctx,next)=>{
+    var name = ctx.request.body.name || '';
+    var password = ctx.request.body.password || '';
+    console.log(`signin with name: ${name}, password: ${password}`);
+    if (name === 'koa' && password === '12345') {
+        ctx.response.body = `<h1>Welcome, ${name}!</h1>`;
+    } else {
+        ctx.response.body = `<h1>Login failed!</h1>
+        <p><a href="/">Try again</a></p>`;
+    }
+});
+
+//由于middleware的顺序很重要，这个koa-bodyparser必须在router之前被注册到app对象
+app.use(bodyParser());
 // add router middleware:
 app.use(router.routes());
+
 
 app.listen(3000);
 console.log('app started at port 3000...');
